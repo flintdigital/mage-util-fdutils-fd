64c84517c1eeef914966d8cb07f9251891f3bcfe
Adrian Esquivel <adrianesquivels@gmail.com>
2016-03-14 12:29:44 -0600

    Update db_cleanup_script.php

--- fdutils/cleanup/db_cleanup_script.php
+++ fdutils/cleanup/db_cleanup_script.php
@@ -27,18 +27,26 @@ function clean_log_tables() {
     global $db;
    
     $tables = array(
-        'dataflow_batch_export',
-        'dataflow_batch_import',
-        'log_customer',
-        'log_quote',
-        'log_summary',
-        'log_summary_type',
-        'log_url',
-        'log_url_info',
-        'log_visitor',
-        'log_visitor_info',
-        'log_visitor_online',
-        'report_event'
+     'aw_core_logger',
+     'dataflow_batch_export',
+     'dataflow_batch_import',
+     'log_customer',
+     'log_quote',
+     'log_summary',
+     'log_summary_type',
+     'log_url',
+     'log_url_info',
+     'log_visitor',
+     'log_visitor_info',
+     'log_visitor_online',
+     'index_event',
+     'report_event',
+     'report_viewed_product_index',
+     'report_compared_product_index',
+     'catalog_compare_item',
+     'catalogindex_aggregation',
+     'catalogindex_aggregation_tag',
+     'catalogindex_aggregation_to_tag'
     );
     
     echo "Connecting to DB\n";
